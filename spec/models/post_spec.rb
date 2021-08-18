c261e54c30d88ce0613aac060db65640974c4d29
Tjandra Darmo <tjandradarmo@protonmail.com>
2021-08-18 21:27:10 +0700

    test: Add test case when post returning 500

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -11,6 +11,7 @@ describe Post do
                                         post_text: "Hi! This is a post #now. And I'm here on #Singapore", attachment: 'some_file.txt')
     @client_response = { 'id' => 1, 'user_id' => 1,
                          'post_text' => "Hi! This is a post #now. And I'm here on #Singapore", 'datetime' => DateTime.now }
+    @invalid_client_response = { 'id' => 1 }
     allow(Mysql2::Client).to receive(:new).and_return(@mock_client)
     allow(@mock_client).to receive(:close)
   end
@@ -23,6 +24,27 @@ describe Post do
         valid_result = post.valid?
 
         expect(valid_result).to eq(false)
+      end
+    end
+
+    context 'when return value is different' do
+      it 'should return 500 status code' do
+        mock_query = "INSERT INTO posts (user_id, post_text) VALUES (#{@post_without_attachment.user_id}, '#{@post_without_attachment.post_text}')"
+        mock_query_get = 'SELECT * FROM posts WHERE id = 1'
+
+        allow(@mock_client).to receive(:last_id).and_return(1)
+        allow(@mock_client).to receive(:query).with(mock_query)
+        allow(@mock_client).to receive(:query).with(mock_query_get).and_return([@invalid_client_response])
+
+        stub_hashtag = double
+
+        allow(Hashtag).to receive(:new).and_return(stub_hashtag)
+        allow(stub_hashtag).to receive(:save)
+        allow(stub_hashtag).to receive(:save_post_hashtag)
+
+        expected_result = @post_without_attachment.save_post
+
+        expect(expected_result).to eq(500)
       end
     end
 
