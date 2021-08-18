c048e390ac40324142d393217fc1590d6e8d205a
Tjandra Darmo <tjandradarmo@protonmail.com>
2021-08-18 21:38:53 +0700

    test: Add test case for when no post were found when call search in PostController

--- spec/controllers/post_controller_spec.rb
+++ spec/controllers/post_controller_spec.rb
@@ -9,28 +9,48 @@ describe PostController do
   end
 
   describe 'save post' do
-    it 'should store post into the database' do
-      stub_model = double
+    context 'when given valid params' do
+      it 'should store post into the database' do
+        stub_model = double
+        mock_post_return = double
 
-      allow(Post).to receive(:new).with(@post_params).and_return(stub_model)
-      allow(stub_model).to receive(:save_post).and_return(200)
+        allow(Post).to receive(:new).with(@post_params).and_return(stub_model)
+        allow(stub_model).to receive(:save_post).and_return(mock_post_return)
+        allow(mock_post_return).to receive(:each)
 
-      controller_result = @post_controller.save(@post_params)
+        controller_result = @post_controller.save(@post_params)
 
-      expect(controller_result).to eq(200)
+        expect(controller_result).to eq({
+                                          'status' => 200,
+                                          'message' => 'Success',
+                                          'post' => mock_post_return
+                                        })
+      end
     end
   end
 
   describe 'search post' do
-    it 'should return posts which contains the desired hashtag' do
-      stub_model = double
+    context 'when a minimum of a post found' do
+      it 'should return posts which contains the desired hashtag' do
+        stub_model = double
 
-      allow(Post).to receive(:search).with('now').and_return(stub_model)
-      allow(stub_model).to receive(:each).and_return('No post matched the hashtag')
+        allow(Post).to receive(:search).with('now').and_return(stub_model)
+        allow(stub_model).to receive(:each)
 
-      controller_result = @post_controller.search(hashtag: 'now')
+        controller_result = @post_controller.search(hashtag: 'now')
 
-      expect(controller_result).to eq({ 'posts' => 'No post matched the hashtag' })
+        expect(controller_result).to eq({ 'posts' => stub_model.each })
+      end
+    end
+
+    context 'when no post were found' do
+      it 'should return custom message in map' do
+        allow(Post).to receive(:search).with('now').and_return(nil)
+
+        controller_result = @post_controller.search(hashtag: 'now')
+
+        expect(controller_result).to eq({ 'posts' => 'No post matched the hashtag' })
+      end
     end
   end
 end
