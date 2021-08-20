a92ec5d46ba9613e3382588cf25064da1a0082b7
Tjandra Darmo <tjandradarmo@protonmail.com>
2021-08-20 21:55:04 +0700

    feat: Create save method inside Attachment class to store attachment into the server

--- models/attachment.rb
+++ models/attachment.rb
@@ -1,8 +1,14 @@
 class Attachment
     attr_reader :filename
-    
+
     def initialize(params)
         @filename = params[:filename]
         @file = params[:tempfile]
+    end
+
+    def save
+        File.open("./public/#{@filename}", "wb") do |f|
+            f.write(@file.read)
+        end
     end
 end
--- spec/models/attachment_spec.rb
+++ spec/models/attachment_spec.rb
@@ -10,4 +10,20 @@ describe Attachment do
             expect(filename).to eq('something.txt')
         end
     end
+
+    describe 'save attachment' do
+        it 'should save attachment to the server' do
+            mock_file = double
+            mock_tempfile = double
+
+            allow(mock_tempfile).to receive(:read)
+
+            attachment = Attachment.new(filename: 'something.txt', tempfile: mock_tempfile)
+
+            allow(File).to receive(:open).with('./public/something.txt', 'wb').and_yield(mock_file)
+            expect(mock_file).to receive(:write)
+
+            attachment.save
+        end
+    end
 end
